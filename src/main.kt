0fee2ef1521c2159a070fb870bc95125c25d704a
afonso.goncalves001@gmail.com <Qui@bo5771>
2022-08-18 21:01:27 -0300

    Add the deposita function

--- src/main.kt
+++ src/main.kt
@@ -21,34 +21,17 @@ fun main() {
     println(contaFran.numero)
     println(contaFran.saldo)
 
-    val numeroX = 10
-    var numeroY = numeroX
-    numeroY++
+    println("Depositando na conta do Alex")
+    deposita(contaAlex, valor = 50.0)
+    println(contaAlex.saldo)
 
-    println("numeroX $numeroX")
-    println("numeroY $numeroY")
+    println("Depositando na conta do Fran")
+    deposita(contaFran, valor = 100.0)
+    println(contaFran.saldo)
+}
 
-/*
-    Referencias, ou seja, espaço reservados na memórian o momento em que definimos em que a conta Mária tinha o valor
-    presente da conta João, o João aponta para conta e por consequência a Maria também aponta para Conta(), quando
-    alteramos alguma coisa presente do titular acabamos alterando o mesmo objeto, tome cuidado na atribuição pois não
-    se trata de uma cópia, mas uma referencia a o espaço da memória
-*/
-
-    val contaJoao = Conta()
-    contaJoao.titular = "João"
-    var contaMaria = contaJoao
-    contaMaria.titular = "Maria"
-
-    println("titular conta joão: ${contaJoao.titular}")
-    println("titular conta Maria: ${contaMaria.titular}")
-
-/*
-    Print da classe + hash da alocação de memória, porém se utilizarmos uma instacia com um novo objeto, ele aponta
-    para um novo espaço de memoria, mudando o hash.
-*/
-    println(contaJoao)
-    println(contaMaria)
+fun deposita(conta: Conta, valor: Double){
+    conta.saldo += valor
 }
 
 class Conta () {
@@ -56,6 +39,7 @@ class Conta () {
     var numero = 0
     var saldo = 0.0
 }
+
 
 fun testaLacos() {
     for (i in 5 downTo 1) {
@@ -71,10 +55,43 @@ fun testaLacos() {
     }
 }
 
+
 fun testaCondicoes(saldo: Double) {
     when {
         saldo > 0.0 -> println("Conta é positiva")
         saldo == 0.0 -> println("Conta é neutra")
         else -> println("Conta negativa")
     }
+}
+
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    /*
+        Referencias, ou seja, espaço reservados na memórian o momento em que definimos em que a conta Mária tinha o valor
+        presente da conta João, o João aponta para conta e por consequência a Maria também aponta para Conta(), quando
+        alteramos alguma coisa presente do titular acabamos alterando o mesmo objeto, tome cuidado na atribuição pois não
+        se trata de uma cópia, mas uma referencia a o espaço da memória
+    */
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    var contaMaria = contaJoao // Substitua para Conta(), para que assim não ocorra erro!
+    contaMaria.titular = "Maria"
+
+    println("titular conta joão: ${contaJoao.titular}")
+    println("titular conta Maria: ${contaMaria.titular}")
+
+    /*
+        Print da classe + hash da alocação de memória, porém se utilizarmos uma instacia com um novo objeto, ele aponta
+        para um novo espaço de memoria, mudando o hash.
+    */
+
+    println(contaJoao)
+    println(contaMaria)
 }
